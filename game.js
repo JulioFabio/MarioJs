1a409dca57dfd6c1ec09cfafd5de49374b479585
JulioFabio <juliofabiodossantosjfs@gmail.com>
2022-09-06 19:47:50 -0300

    Changes to be committed: 	modified:   game.js

--- game.js
+++ game.js
@@ -8,30 +8,42 @@ kaboom({
 loadRoot("https://i.imgur.com/")
 
 loadSprite('bloco','M6rwarW.png')
+loadSprite('goomba','KPO3fR9.png')
+loadSprite('surpresa','gesQ1KP.png')
+loadSprite('unboxed','bdrLpi6.png')
+loadSprite('moeda','wbKxhcd.png')
+loadSprite('mario','Wb1qfhK.png')
+loadSprite('cogumelo','0wMd92p.png')
 
 scene("game", () => {
     layer(["bg", "obj", "ui"], "obj")
 
     const map = [
-        '                                   ',
-        '                                   ',
-        '                                   ',
-        '                                   ',
-        '                                   ',
-        '                                   ',
-        '                                   ',
-        '                                   ',
-        '                                   ',
-        '                                   ',
-        '                                   ',
-        '                                   ',
-        '===================================',
+        '=                                   =',
+        '=                                   =',
+        '=                                   =',
+        '=                                   =',
+        '=                                   =',
+        '=                                   =',
+        '=                                   =',
+        '=                                   =',
+        '=                                   =',
+        '=                                   =',
+        '=                                   =',
+        '=                                   =',
+        '=====================================',
     ]
 
     const levelCfg = {
         width: 20,
         height: 20,
-        '=': [sprite('bloco'), solid()]
+        '=': [sprite('bloco'), solid()],
+        '$': [sprite('moeda'), 'moeda'],
+        '%': [sprite('surpresa'), solid(), 'moeda-surpresa'],
+        '*': [sprite('surpresa'), solid(), 'cogumelo-surpresa'],
+        '{': [sprite('unboxed'), solid()],
+        '^': [sprite('goomba'), 'dangerous'],
+        '#': [sprite('cogumelo'), 'cogumelo',body()],
     }
 
     const gameLevel = addLevel(map, levelCfg)
